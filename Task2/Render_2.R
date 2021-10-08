05d9dab1a48d76c4eb73bb7a620d7cf2c4453f42
alexaoh <alexaoh@users.noreply.github.com>
2021-10-08 22:48:23 +0200

    Almost completed Task2 (accession.number not 100%) and
    something wrong with my Render-function option "all" (the name I give as
    a parameter does not "stick" to the pdf file, only the html file). Not
    sure why this is happening now.

---
+++ Task2/Render_2.R
@@ -0,0 +1,14 @@
+render.report = function(path, fasta_name, output_filename, output_fileformat){
+  rmarkdown::render(
+    "/home/ajo/gitRepos/bioinformatics/Task2/SeqAna_2.Rmd", params = list(
+      path = path, 
+      name_fasta = fasta_name
+    ),
+    output_file = output_filename,
+    output_format = if (output_fileformat == "all") "all" else paste(output_fileformat, "document", sep = "_") 
+    # Can specify only one output format or "all" for both html and pdf. 
+  )
+}
+
+render.report("/home/ajo/gitRepos/bioinformatics/Task2", "ENSG00000114374.fa", "SeqAna_2_ENSG00000114374", "all")
+
