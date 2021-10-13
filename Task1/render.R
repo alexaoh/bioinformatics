526b358df96c5ed20422245dcfe3f28c6b7e41fc
alexaoh <alexaoh@users.noreply.github.com>
2021-10-13 20:50:02 +0200

    Did most important things in Task1-2 and Task2 (could do more)

--- Task1/render.R
+++ Task1/render.R
@@ -6,14 +6,18 @@ render.report = function(path, fasta_name, output_filename, output_fileformat){
     ),
     output_file = output_filename,
     output_format = if (output_fileformat == "all") "all" else paste(output_fileformat, "document", sep = "_") 
-    # Can specify only one output format or "all" for both html and pdf. 
+    # Can specify only one output format or "all" for both html and pdf. Does not give the correct names to both pdf and html now, not sure why. 
   )
 }
 
-# My Latex engine cannot render the output table from the DNA. Not sure how to fix this right now. 
-# Thus, I am using the option "html" below and not "all". 
-# Please try will "all" or "pdf" to see if you have the same problem with the latex engine as I am having. 
+render.report("/home/ajo/gitRepos/bioinformatics/Task1", "sequence3.fa", "SeqAna3", "pdf")
 render.report("/home/ajo/gitRepos/bioinformatics/Task1", "sequence3.fa", "SeqAna3", "html")
+
+render.report("/home/ajo/gitRepos/bioinformatics/Task1", "sequence4.fa", "SeqAna4", "pdf")
 render.report("/home/ajo/gitRepos/bioinformatics/Task1", "sequence4.fa", "SeqAna4", "html")
+
+render.report("/home/ajo/gitRepos/bioinformatics/Task1", "gi32141095_N_1.fa", "SeqAna1", "pdf")
 render.report("/home/ajo/gitRepos/bioinformatics/Task1", "gi32141095_N_1.fa", "SeqAna1", "html")
+
+render.report("/home/ajo/gitRepos/bioinformatics/Task1", "gi32141095_N_0.fa", "SeqAna0", "pdf")
 render.report("/home/ajo/gitRepos/bioinformatics/Task1", "gi32141095_N_0.fa", "SeqAna0", "html")
--- Task2/Render_2.R
+++ Task2/Render_2.R
@@ -4,11 +4,17 @@ render.report = function(path, fasta_name, output_filename, output_fileformat){
       path = path, 
       name_fasta = fasta_name
     ),
-    output_file = output_filename,
+    output_file = paste(output_filename,format(Sys.Date(),'%Y-%m-%d'), sep = "_"),
     output_format = if (output_fileformat == "all") "all" else paste(output_fileformat, "document", sep = "_") 
-    # Can specify only one output format or "all" for both html and pdf. 
+    # "all" does not give the correct names to both pdf and html here either. 
   )
 }
 
-render.report("/home/ajo/gitRepos/bioinformatics/Task2", "ENSG00000114374.fa", "SeqAna_2_ENSG00000114374", "pdf")
-render.report("/home/ajo/gitRepos/bioinformatics/Task2", "ENSG00000114374.fa", "SeqAna_2_ENSG00000114374", "html")
+render.report("/home/ajo/gitRepos/bioinformatics/Task2/fastas", "ENSG00000114374.fa", "ENSG00000114374", "html")
+render.report("/home/ajo/gitRepos/bioinformatics/Task2/fastas", "ENSG00000114374.fa", "ENSG00000114374", "pdf")
+
+render.report("/home/ajo/gitRepos/bioinformatics/Task2/fastas", "ENSG00000169800.fa", "ENSG00000169800", "html")
+render.report("/home/ajo/gitRepos/bioinformatics/Task2/fastas", "ENSG00000169800.fa", "ENSG00000169800", "pdf")
+
+render.report("/home/ajo/gitRepos/bioinformatics/Task2/fastas", "ENSG00000169953.fa", "ENSG00000169953", "html")
+render.report("/home/ajo/gitRepos/bioinformatics/Task2/fastas", "ENSG00000169953.fa", "ENSG00000169953", "pdf")
