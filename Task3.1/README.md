156ae3265c343d3a58912977a63b9d31a481d819
alexaoh <alexaoh@users.noreply.github.com>
2021-11-10 20:56:05 +0100

    Completed Task3.1 (Good enough, could always do more)

---
+++ Task3.1/README.md
@@ -0,0 +1,5 @@
+# Files in the Directory
+
+* StatSignfPairSeqAlign (Directory): Package directory. Contains .Rproj, Namespace, Description, R code and help file.
+* outputHelpExamples.*: These files contain the output from the examples listed in the help file, under "Examples". The .Rmd file is the source code and there are two reports (one .html and one .pdf)
+* dashboard.*: Contains a flexdashboard with different examples of use (Friendly Output: Reports). The .Rmd file is the source code and the .html is the dashboard for display. 
--- Task3.1/StatSignfPairSeqAlign/R/statSignf.R
+++ Task3.1/StatSignfPairSeqAlign/R/statSignf.R
@@ -1,14 +1,17 @@
 # Create an R function to show the statistical significance of a pairwise alignment.
 
 statSignf <- function(seq1, seq2, kind.seq = "Protein", kind.align = "global", subst.matrix = "BLOSUM62",
-                      gap.scores = c(3, 1), num.shuffles = 1000, shuffle.seq = 1, plot = TRUE){
+                      gap.scores = c(3, 1), num.shuffles = 1000, shuffle.seq = 1, plot = TRUE,
+                      suppress.output = FALSE){
 
   # Should probably have some form of asserts on the input parameters.
 
-  # Load the dependencies inside function for now.
-  library(seqinr)
-  library(Biostrings)
-  data(list = subst.matrix)
+  # Load the dependencies inside function for now. Should not be done like this.
+  #library(seqinr)
+  #library(Biostrings)
+  options(warn=-1)
+  data(list = subst.matrix) # This works, but shows warning (think it because of the imports when making the package).
+  options(warn=0)
 
   # Split the fastas into one name-part and one sequence-part
   string1.split <- strsplit(toString(AAStringSet(seq1)), "\n")[[1]]
@@ -27,13 +30,24 @@ statSignf <- function(seq1, seq2, kind.seq = "Protein", kind.align = "global", s
   n <- nchar(s2)
 
   if (kind.align == "global"){
-    OG.score <- pairwiseAlignment(s1, s2, substitutionMatrix = subst.matrix,
-                                  gapOpening = gap.scores[1], gapExtension = gap.scores[2],
-                                  scoreOnly = TRUE, type = "global")
+    if (kind.seq == "Protein"){
+      OG.score <- pairwiseAlignment(s1, s2, substitutionMatrix = subst.matrix,
+                                    gapOpening = gap.scores[1], gapExtension = gap.scores[2],
+                                    scoreOnly = TRUE, type = "global")
+    } else {
+      OG.score <- pairwiseAlignment(s1, s2, gapOpening = gap.scores[1],
+                                    gapExtension = gap.scores[2], scoreOnly = TRUE, type = "global")
+    }
   } else {
-    OG.score <- pairwiseAlignment(s1, s2, substitutionMatrix = subst.matrix,
+    if (kind.seq == "Protein"){
+      OG.score <- pairwiseAlignment(s1, s2, substitutionMatrix = subst.matrix,
                                   gapOpening = gap.scores[1], gapExtension = gap.scores[2],
                                   scoreOnly = TRUE, type = "local")
+    } else {
+      OG.score <- pairwiseAlignment(s1, s2,
+                                    gapOpening = gap.scores[1], gapExtension = gap.scores[2],
+                                    scoreOnly = TRUE, type = "local")
+    }
   }
 
   # This calculates scores with random permutations of one of the sequences, in order to analyse statistical significance.
@@ -51,15 +65,27 @@ statSignf <- function(seq1, seq2, kind.seq = "Protein", kind.align = "global", s
     }
 
     if (kind.align == "global"){
+      if (kind.seq == "Protein"){
       scores[i] <- pairwiseAlignment(x, x2,
                                      substitutionMatrix = subst.matrix,
                                      gapOpening = gap.scores[1], gapExtension = gap.scores[2],
                                      scoreOnly = TRUE, type = "global")
+      } else {
+        scores[i] <- pairwiseAlignment(x, x2,
+                                       gapOpening = gap.scores[1], gapExtension = gap.scores[2],
+                                       scoreOnly = TRUE, type = "global")
+      }
     } else {
+      if (kind.seq == "Protein"){
       scores[i] <- pairwiseAlignment(x, x2,
                                      substitutionMatrix = subst.matrix,
                                      gapOpening = gap.scores[1], gapExtension = gap.scores[2],
                                      scoreOnly = TRUE, type = "local")
+      } else {
+        scores[i] <- pairwiseAlignment(x, x2,
+                                       gapOpening = gap.scores[1], gapExtension = gap.scores[2],
+                                       scoreOnly = TRUE, type = "local")
+      }
     }
 
   }
@@ -95,49 +121,59 @@ statSignf <- function(seq1, seq2, kind.seq = "Protein", kind.align = "global", s
     lambda.hat*exp(-lambda.hat*(x-u.hat)-exp(-lambda.hat*(x-u.hat)))
   }
 
+
+  if (suppress.output == FALSE){
+    # Print the summary of the function (could be formatted more nicely later probably).
+    cat("The names of the sequences are '", substr(name1, start = 2, stop = nchar(name1)-1), "' and \n'",
+        substr(name2, start = 2, stop = nchar(name2)-1), "'. \n", sep = "")
+    cat("These sequences are of type '", kind.seq, "'. \n", sep = "")
+    cat("The type of alignment that has been done here is '", kind.align, "'. \n", sep = "")
+    if (kind.seq == "Protein"){
+      cat("The substitution matrix used is '", subst.matrix, "'. \n", sep = "")
+    }
+    cat("The gap scores are -", gap.scores[1], " for open penalty and -", gap.scores[2], " for extended penalty. \n", sep = "")
+    cat("The number of shuffles done are ", num.shuffles, ". \n", sep = "")
+    if (shuffle.seq == 1){
+      cat("The shuffling was done on the first sequence mentioned above. \n")
+    } else { # Just as earlier, the other option for shuffle.seq has to be 2!
+      cat("The shuffling was done on the second sequence mentioned above. \n")
+    }
+
+    cat("The original score is ", OG.score, ". \n", sep = "")
+    cat("The estimations of the parameters of the Gumbel distribution are: \n Scale parameter lambda = ", lambda.hat,
+        " and mode u = ", u.hat, ". \n", sep = "")
+    cat("The p-value estimated using the estimated Gumbel distribution is ", p.gumbel, ". \n", sep = "")
+    cat("The p-value estimated empirically by counting in the histogram is ", p.counted, ". \n", sep = "")
+
+    cat("The estimated K is ", K.hat, ". \n", sep = "")
+    cat("The standardized score is S' = ", standard.score, ". \n", sep = "")
+
+    # Summary.
+    cat("Summary of the", num.shuffles, "scores calculated after shuffling: \n")
+    cat(format(c("Min.", "1st Qu.", "Median", "Mean", "3rd Qu.", "Max"), width = 8, justify = "left"))
+    cat("\n")
+    cat(format(c(score.min, score.1st.quant, score.median, score.mean, score.3rd.quant, score.max),
+               width = 8, justify = "left"))
+    cat("\n")
+  }
+
   if (plot){
-    par(mar = c(4, 4, 6, 4))
-    x <- seq(from = min(scores)-1, to = max(scores) + 1, along.with = scores) # make x-axis for Gumbel.
-    gumbel <- gumbel.dist(x) # Calculated Gumbel curve.
-    hist(scores, freq = F, ylim = c(0, max(gumbel))) # plot the histogram.
-    lines(x, gumbel, col = "blue") # plot the estimated Gumbel alongside it.
-    abline(v = OG.score, col = 2, lty = 2) # Add line for the original score on the plot.
-    legend("topleft", legend = c("Gumbel", "Original Score"), col = c("blue", 2), lty = 1:2)
-    # mtext(paste0("Names: ", substr(name1, start = 2, stop = nchar(name1)-1)
-    #              , " and ", substr(name2, start = 2, stop = nchar(name2)-1)), side = 3)
-    mtext(paste0("Gap Scores: -", gap.scores[1]," for open penalty and -", gap.scores[2], " for extended penalty. \n"))
-    #mtext(paste0(num.shuffles, " shuffles. \n"), side = 4, adj = 1, padj = 0)
-    #mtext(paste0("Original Score: ", OG.score, "\n"))
-    mtext(paste0("Parametric p-value ", round(p.gumbel, 3), " and counted p-value: ", round(p.counted, 3)))
+    plot <- function(){
+      par(mar = c(4, 4, 6, 4))
+      x <- seq(from = min(scores)-1, to = max(scores) + 1, along.with = scores) # make x-axis for Gumbel.
+      gumbel <- gumbel.dist(x) # Calculated Gumbel curve.
+      hist(scores, freq = F, ylim = c(0, max(gumbel))) # plot the histogram.
+      lines(x, gumbel, col = "blue") # plot the estimated Gumbel alongside it.
+      abline(v = OG.score, col = 2, lty = 2) # Add line for the original score on the plot.
+      legend("topleft", legend = c("Gumbel", "Original Score"), col = c("blue", 2), lty = 1:2)
+      # mtext(paste0("Names: ", substr(name1, start = 2, stop = nchar(name1)-1)
+      #              , " and ", substr(name2, start = 2, stop = nchar(name2)-1)), side = 3)
+      mtext(paste0("Gap Scores: -", gap.scores[1]," for open penalty and -", gap.scores[2], " for extended penalty. \n"))
+      #mtext(paste0(num.shuffles, " shuffles. \n"), side = 4, adj = 1, padj = 0)
+      #mtext(paste0("Original Score: ", OG.score, "\n"))
+      mtext(paste0("Parametric p-value ", round(p.gumbel, 3), " and counted p-value: ", round(p.counted, 3)))
+    }
+    return (plot)
   }
-
-  # Print the summary of the function (could be formatted more nicely later probably).
-  cat("The names of the sequences are '", substr(name1, start = 2, stop = nchar(name1)-1), "' and '",
-      substr(name2, start = 2, stop = nchar(name2)-1), "'. \n", sep = "")
-  cat("These sequences are of type '", kind.seq, "'. \n", sep = "")
-  cat("The type of alignment that has been done here is '", kind.align, "'. \n", sep = "")
-  cat("The substitution matrix used is '", subst.matrix, "'. \n", sep = "")
-  cat("The gap scores are -", gap.scores[1], " for open penalty and -", gap.scores[2], " for extended penalty. \n", sep = "")
-  cat("The number of shuffles done are ", num.shuffles, ". \n", sep = "")
-  if (shuffle.seq == 1){
-    cat("The shuffling was done on the first sequence mentioned above. \n")
-  } else { # Just as earlier, the other option for shuffle.seq has to be 2!
-    cat("The shuffling was done on the second sequence mentioned above. \n")
-  }
-
-  cat("The original score is ", OG.score, ". \n", sep = "")
-  cat("The estimations of the parameters of the Gumbel distribution are: Scale parameter lambda = ", lambda.hat,
-      " and mode u = ", u.hat, ". \n", sep = "")
-  cat("The p-value estimated using the estimated Gumbel distribution is ", p.gumbel, ". \n", sep = "")
-  cat("The p-value estimated empirically by counting in the histogram is ", p.counted, ". \n", sep = "")
-
-  cat("The estimated K is ", K.hat, ". \n", sep = "")
-  cat("The standardized score is S' = ", standard.score, ". \n", sep = "")
-
-  # Summary.
-  cat("Summary of the", num.shuffles, "scores calculated after shuffling: \n")
-  cat("Min.\t1st Qu.\tMedian\tMean\t3rd Qu.\tMax.\n")
-  cat(paste(score.min, score.1st.quant, score.median, score.mean, score.3rd.quant, score.max,sep="\t"))
-  cat("\n")
 
 }
